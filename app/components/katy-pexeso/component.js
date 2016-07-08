2ec44f7761c54a44b0ea8726375e53623cd4d1f8
Michal Bryxí <michal.bryxi@gmail.com>
2016-07-08 22:05:57 +0100

    Pretty nice grid

--- app/components/katy-pexeso/component.js
+++ app/components/katy-pexeso/component.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  width: 10,
-  height: 10,
+  width: 3,
+  height: 3,
+  base: 150,
 
   seed: Ember.computed(function () {
     return Math.floor(Math.random() * 100);
@@ -11,22 +12,34 @@ export default Ember.Component.extend({
   tiles: Ember.computed('width', 'height', 'seed', function () {
     let width = this.get('width');
     let height = this.get('height');
+    let base = this.get('base');
+    let id;
     let tiles = [];
 
     for (let i = 0; i < width; i++) {
       for (let j = 0; j < height; j++) {
-        let w =  Math.floor(Math.random() * 5) + 1;
-        let h =  Math.floor(Math.random() * 5) + 1;
+        let id =  Math.floor(Math.random() * 9999) + 1;
+        let tile = {
+          id: id,
+        };
 
-        tiles.push(
-          {
-            colspan: w,
-            rowspan: h,
-            img_w: w * 100,
-            img_h: h * 100,
-          }
-        );
+        tiles.push(tile);
+        tiles.push(tile);
       }
+    }
+
+    return tiles;
+  }),
+
+  tilesShuffled: Ember.computed('tiles.[]', function () {
+    let tiles = this.get('tiles');
+    let j = this.get('width') * this.get('height');
+
+    while (j > 0) {
+      let i = Math.floor(Math.random() * j--);
+      let temp = tiles[i];
+      tiles[i] = tiles[j];
+      tiles[j] = temp;
     }
 
     return tiles;
